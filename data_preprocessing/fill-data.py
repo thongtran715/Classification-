bbe6652d1a875c8afbcb522ad2459d63cef3d34b
thongtran715 <thongtran715@gmail.com>
2018-03-23 14:35:33 -0400

    This is the first

---
+++ data_preprocessing/fill-data.py
@@ -0,0 +1,102 @@
+import fileinput
+import sys
+read_file = open("data.txt", "r+")
+
+
+def fill_average(name_file, index):
+	f = open(name_file, 'r')
+	sum = 0.0
+	tracker = 0
+	for lines in f:
+		lines = lines.split(",")
+		counter = 0
+		for line in lines:
+			if counter == index:
+				if line != "?":
+					sum += float(line)
+				else:
+					tracker -= 1
+
+			counter += 1
+		tracker += 1
+	result = sum/tracker
+	f.close()
+	return result
+
+def perform_median (data_set):
+	data_set.sort()
+	length = len(data_set)
+	if length % 2 == 0: #this is the even
+		mid = floor(length/2)
+		return data_set[mid]
+	else:
+		mid_next = int(length/2)
+		mid_prev = mid_next -1 
+		return (float(data_set[mid_next]) + float(data_set[mid_prev]))/2
+	return 0
+
+def fill_median (name_file, index):
+	f = open(name_file,'r')
+	data_set = []
+	for line in f:
+		line = line.split(",")
+		counter = 0 
+		for data in line:
+			if counter == index:
+				if data != "?":
+					data_set.append(data)
+			counter += 1
+	return perform_median(data_set)
+
+#this method is to replace every missing value with median
+try:
+	lst = []
+	for line in read_file:
+		line = line.split(',')
+		counter = 0
+		for data in line:
+			if data == "?":
+				number = fill_median("data.txt", counter)
+				lst.append(number)
+				print(number)
+				break
+			counter += 1
+finally:
+	read_file.close()
+
+index = 0
+for line in fileinput.input("data.txt", inplace=1):
+		if "?" in line:
+			replace_text = str(lst[index])
+			line = line.replace("?",replace_text)
+			index += 1
+		sys.stdout.write(line)
+# This is the method to replace every missing value with average. Seems not to work well
+"""
+try:
+
+	lst = []
+	for line in read_file:
+		line = line.split(',')
+		counter = 0
+		for data in line:
+			if data == "?":
+				number = fill_average("data.txt", counter)
+				print(number)
+				lst.append(number)
+				break
+			counter += 1
+
+finally:
+	read_file.close()
+
+index = 0
+for line in fileinput.input("data.txt", inplace=1):
+		if "?" in line:
+			replace_text = str(lst[index])
+			line = line.replace("?",replace_text)
+			index += 1
+		sys.stdout.write(line)
+"""
+
+
---
+++ data_preprocessing/re-format-data.py
@@ -0,0 +1,29 @@
+#this file is to re-format the data
+read_file = open("back_up_data.txt", 'r')
+write_file = open("complete_data.txt", 'a')
+
+for lines in read_file:
+	lines = lines.strip() # Chop the end line
+	lines = lines.split(",")
+	#Switch the array with last column
+	last = lines[len(lines) -1]
+	lines[len(lines) - 1] = lines[0]
+	lines[0] = last
+	#create the str to hold the format
+	string = ""
+	for i in range(1,len(lines)):
+		index = str(i)
+		value = str(lines[i])
+		combine = index + ":" + value
+		string = string + combine + " "
+	if lines[0] == '0':
+		append_string = "+1" + " "
+		append_string += string
+	else:
+		append_string = "-1" + " "
+		append_string += string
+	append_string+="\n"
+	write_file.write(append_string)
+
+read_file.close()
+write_file.close()
